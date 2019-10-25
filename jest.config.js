9cf3574fa276a8cbf3a3a1a9fde505914416e706
progfay <shun1856shun@gmail.com>
2019-10-26 01:16:59 +0900

    test: add snapshot test with jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json'
+    }
+  },
+  testMatch: [
+    '**/tests/**/*.test.ts'
+  ]
+}
---
+++ tests/index.test.ts
@@ -0,0 +1,14 @@
+/* global describe it expect */
+
+import * as fs from 'fs'
+import { parse as parseScrapbox } from '@progfay/scrapbox-parser'
+import convert from '../src'
+import pretty from 'pretty'
+
+describe('Page', () => {
+  it('Sample page', () => {
+    const page = fs.readFileSync('./tests/page.txt').toString()
+  const html = convert(parseScrapbox(page), 'projectName')
+  expect(pretty(html)).toMatchSnapshot()
+  })
+})
