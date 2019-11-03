e12fcb9a1fdc4cd8803adf46b66462d4241215fa
progfay <shun1856shun@gmail.com>
2019-11-03 17:29:55 +0900

    feat(googleMap): use `<amp-iframe>`

--- src/convertBlockToHTML/line/googleMap.tsx
+++ src/convertBlockToHTML/line/googleMap.tsx
@@ -2,14 +2,16 @@ import { pragma } from 'amphtml-tsx'
 import { GoogleMapNodeType } from '@progfay/scrapbox-parser'
 import { NodeConverterType } from '.'
 
+const GOOGLE_MAP_API_KEY = 'AIzaSyC2sPsKnxrPTmqLbqEK2SVOiVzSCyaDmGg'
+
 const GoogleMapNodeConverter: NodeConverterType<GoogleMapNodeType> = ({ latitude, longitude, zoom, place }) => (
-  <iframe
+  <amp-iframe
     class='googlemap'
-    src={`https://maps.google.co.jp/maps?output=embed&ll=${latitude},${longitude}&z=${zoom}&q=${encodeURIComponent(place)}`}
-    width={620}
-    height={450}
-    frameborder={0}
-    allowfullscreen />
+    width="620" height="450"
+    sandbox="allow-scripts allow-same-origin"
+    layout="responsive"
+    frameborder="0"
+    src={`https://www.google.com/maps/embed/v1/place?key=${GOOGLE_MAP_API_KEY}&ll=${latitude},${longitude}&z=${zoom}&q=${encodeURIComponent(place)}`} />
 )
 
 export default GoogleMapNodeConverter
