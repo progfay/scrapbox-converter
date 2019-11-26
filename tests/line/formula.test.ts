d6ea42d321f9058e547394e11e91d05be7c16281
progfay <shun1856shun@gmail.com>
2019-11-27 00:21:25 +0900

    fix(test): escape back slash

--- tests/line/formula.test.ts
+++ tests/line/formula.test.ts
@@ -7,7 +7,7 @@ import pretty from 'pretty'
 describe('formula', () => {
   it('Simple formula', () => {
     const input = `Formula
-[$ \sum_{k=0}^N k^2 = \frac{1}{2}N(N + 1)]`
+[$ \sum_{k=0}^N k^2 = \\frac{1}{2}N(N + 1)]`
     const html = convert(parse(input), 'projectName')
     expect(pretty(html)).toMatchSnapshot()
   })
