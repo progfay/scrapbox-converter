3b45cb2aa24f6e999d25b6dfd2b92be00e451044
progfay <shun1856shun@gmail.com>
2019-11-28 01:53:04 +0900

    test: up coverage of codeBlock

--- tests/codeBlock.test.ts
+++ tests/codeBlock.test.ts
@@ -7,12 +7,20 @@ import pretty from 'pretty'
 describe('codeBlock', () => {
   it('Simple codeBlock', () => {
     const input = `Code Block
-code:js
+code:index.js
  function () {
    alert(document.location.href)
    console.log("hello")
    // You can also write comments!
  }`
+    const html = convert(parse(input), 'projectName')
+    expect(pretty(html)).toMatchSnapshot()
+  })
+
+  it('CodeBlock with not registered language', () => {
+    const input = `Code Block
+code:hoge
+ this.language.is()`
     const html = convert(parse(input), 'projectName')
     expect(pretty(html)).toMatchSnapshot()
   })
